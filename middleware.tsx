edaf4042f0ebd514ce0c03cdd857bba31646d398
Md Arif <arif.20527@sscbs.du.ac.in>
2024-12-28 22:06:41 +0530

    trying to fix production at vercel 3

--- middleware.tsx
+++ middleware.tsx
@@ -1,8 +1,2 @@
 export { auth as middleware } from "@/auth"
 
-export const config = {
-    matcher: '/',
-    unstable_allowDynamic: [
-      "**/node_modules/@react-email*/**/*.mjs*",
-    ],
-  };
