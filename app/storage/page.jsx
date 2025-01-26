80f1cffc1bf042ed872e61e4264983a6150875ee
Md Arif <arif.20527@sscbs.du.ac.in>
2025-01-26 17:36:06 +0530

    prod error resolved 3

--- app/storage/page.jsx
+++ app/storage/page.jsx
@@ -6,10 +6,10 @@ const Page = () => {
     const {user}= useContext(Context)
     const [folder,setFolder]= useState([]);
     useEffect(()=>{
-        fetch("/api/getrootfolder",{
-            method:"POST",
-            body:JSON.stringify({user:user.id})
-        }).then((d)=>d.json()).then((d)=>console.log(d.data))
+        // fetch("/api/getrootfolder",{
+        //     method:"POST",
+        //     body:JSON.stringify({user:user.id})
+        // }).then((d)=>d.json()).then((d)=>console.log(d.data))
         // fetch("/api/")
     },[])
   return (
