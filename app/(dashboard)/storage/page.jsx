d8fc2942fc2061b836f8eee93c32e8f8a2a1a113
Md Arif <arif.20527@sscbs.du.ac.in>
2025-01-28 01:25:23 +0530

    working on stoarage root fetch folder and file fetch completed

--- app/(dashboard)/storage/page.jsx
+++ app/(dashboard)/storage/page.jsx
@@ -1,35 +1,82 @@
-"use client"
+"use client";
 
-import { Context } from '@/app/_context/NoteContext'
-import React, { useContext, useEffect, useState } from 'react'
+import { Context } from "@/app/_context/NoteContext";
+import { createFolder } from "@/lib/actions/storageActions";
+import React, { useContext, useEffect, useState } from "react";
 
 const Page = () => {
-  const {user}= useContext(Context)
-  const [location , setLocation ]= useState([]);
-  useEffect(()=>{
-    if(user){
-      fetch("/api/getrootfolder",{
+  const { user } = useContext(Context);
+  const [pathFolders, setPathFolders] = useState([]);
+  const [pos, setPos] = useState(-1);
+  const [newFolder,setNewFolder]= useState(null)
+  const [refetch,setRefetch]= useState(false)
+  const [folders, setFolders] = useState(null);
+  const [files, setFiles] = useState(null);
+
+  useEffect(() => {
+    if (user) {
+      fetch("/api/getrootfolder", {
+        method: "POST",
+        body: JSON.stringify({ user: user.id }),
+      })
+        .then((d) => d.json())
+        .then((d) => {
+          setPathFolders((f) => [...f, d.data]);
+          setPos(() => 0);
+        });
+    }
+    return () => {
+      setPathFolders((f) => []);
+    };
+  }, [user]);
+  useEffect(() => {
+    if (pos >= 0) {
+      console.log(pathFolders)
+      console.log(pos)
+      fetch("/api/getfolders", {
+        method: "POST",
+        body: JSON.stringify({ folder: pathFolders[pos]._id }),
+      })
+        .then((d) => d.json())
+        .then((d) => {
+          console.log("folders",d);
+          setFolders(d.data);
+        });
+      fetch("/api/getfiles",{
         method:"POST",
-        body:JSON.stringify({user:user.id})
+        body:JSON.stringify({folder:pathFolders[pos]._id})
       }).then(d=>d.json()).then((d)=>{
-        console.log(d);
-        setLocation((l)=>[...l,d.data._id])
-        console.log(location)
+        console.log("files",d.data);
+        setFiles(d.data)
       })
     }
-    return ()=>{
-      console.log("Owuvdy")
-      setLocation((l)=>[])
-    }
-  },[user])
-
+  }, [pos,refetch]);
   return (
-    <div>page {location.map((e)=>{
-      return (
-        <>{e}</>
-      )
-    })}</div>
-  )
-}
+    <>
+      <input type="text" onChange={(e)=>{
+        setNewFolder(e.target.value);
+      }}/>
+      <button onClick={async ()=>{
+        await createFolder(newFolder,pathFolders[pos]._id,user.id)
+        setRefetch((s)=>!s)
+      }}>New Folder </button>
+      {folders && folders.map((e) => {
+        return <div key={e._id} onClick={()=>{
+          setPathFolders((f)=>[...f,e]);
+          setPos((p)=>p+1);
+        }}>{e.name}</div>;
+      })}
+      {files && 
+        files.map((e)=>{
+          return (
+            <div key={e._id}>
+              {e.file.name}
+            </div>
+          )
+        })
+      }
+    </>
+  );
+};
 
-export default Page
+export default Page;
--- lib/actions/storageActions.tsx
+++ lib/actions/storageActions.tsx
@@ -1,5 +1,14 @@
 "use server"
 
+import { UserFolder } from "@/models/models"
+
 
 // forwardMedia Implementation left 
 
+export async function createFolder(name:String,parentfolder:String,user:String){
+    await UserFolder.create({
+        name:name,
+        parentfolder:parentfolder,
+        user:user
+    })
+}
