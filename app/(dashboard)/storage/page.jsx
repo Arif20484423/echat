d256580d65f6c500784533f063b101d04319351d
Md Arif <arif.20527@sscbs.du.ac.in>
2025-01-27 23:26:54 +0530

    chats working after (dashboard)-> chat added storage(page and getrootfolder) working on it

--- app/(dashboard)/storage/page.jsx
+++ app/(dashboard)/storage/page.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import { Context } from '@/app/_context/NoteContext'
+import React, { useContext, useEffect, useState } from 'react'
+
+const Page = () => {
+  const {user}= useContext(Context)
+  const [location , setLocation ]= useState([]);
+  useEffect(()=>{
+    if(user){
+      fetch("/api/getrootfolder",{
+        method:"POST",
+        body:JSON.stringify({user:user.id})
+      }).then(d=>d.json()).then((d)=>{
+        console.log(d);
+        setLocation((l)=>[...l,d.data._id])
+        console.log(location)
+      })
+    }
+    return ()=>{
+      console.log("Owuvdy")
+      setLocation((l)=>[])
+    }
+  },[user])
+
   return (
-    <div>page</div>
+    <div>page {location.map((e)=>{
+      return (
+        <>{e}</>
+      )
+    })}</div>
   )
 }
 
-export default page
+export default Page
