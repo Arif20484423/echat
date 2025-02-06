17c814ef08293a80435a76b1c74fa879dc5b9908
Md Arif <arif.20527@sscbs.du.ac.in>
2025-02-06 23:19:31 +0530

    login connected to backend

--- app/user/signin/_Components/Login.tsx
+++ app/user/signin/_Components/Login.tsx
@@ -1,32 +1,43 @@
-"use client"
+"use client";
 import styles from "../../User.module.css";
 import FormContainer from "../../../_UIComponents/FormContainer";
 import InputLabel from "../../../_UIComponents/InputLabel";
 import Button from "../../../_UIComponents/Button";
-import { useActionState } from "react";
-import { userSignIn } from "@/lib/actions/userActions";
-import {ResponseType} from "@/lib/actions/userActions"
-import {FaGoogle,FaGithub}  from "react-icons/fa"
+import { useFormState } from "react-dom";
+import { userSignIn ,userSignInGoogle,userSignInGithub} from "@/lib/actions/userActions";
+import { FaGoogle, FaGithub } from "react-icons/fa";
+import { useEffect, useState } from "react";
 export default function Login() {
-  const initialState={message:null,errors:{}}
-    const [state,formAction]=useActionState(userSignIn,initialState)
-    
+  const initialState = { message: null, errors: {} };
+  const [state, formAction, isPending] = useFormState(userSignIn, initialState);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    setError(true);
+    setTimeout(() => {
+      setError(false);
+    }, 3000);
+  }, [state]);
   return (
     <>
       <FormContainer>
-      <form action={formAction}>
-        <h2 className={styles.head}>Login to eChat</h2>
-        <InputLabel tag="Email" tagname="email"/>
-        <InputLabel tag="Password" tagname="password"/>
-        {/* {state?.message && } */}
-        <Button tag="Submit" type="submit"/>
-        <div className={styles.icons}>
-        <FaGoogle size={25}/>
-        <FaGithub size={25}/>
-        </div>
+        <form action={formAction}>
+          <h2 className={styles.head}>Login to eChat</h2>
+          <InputLabel tag="Email" name="email" />
+          <InputLabel tag="Password" name="password" />
+
+          {error && state?.message}
+
+          <Button tag="Submit" type="submit" />
+          <div className={styles.icons}>
+            <FaGoogle size={25} onClick={()=>{
+              userSignInGoogle()
+            }}/>
+            <FaGithub size={25} onClick={()=>{
+              userSignInGithub()
+            }}/>
+          </div>
         </form>
       </FormContainer>
     </>
   );
 }
-
--- auth.ts
+++ auth.ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
-import { GitHubProfile } from "next-auth/providers/github";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
