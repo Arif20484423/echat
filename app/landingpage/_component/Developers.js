d2f8076667115004fd7c95443cc1ae0a4725ee41
the-sudip <mailofsudipdas@gmail.com>
2025-07-06 01:57:38 +0530

    landing page half complete

---
+++ app/landingpage/_component/Developers.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DevCard from "./DevCard";
+
+const Developers = ({data}) => {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 p-5'>
+      {data.map((devData) => (
+        <div key={devData.id}><DevCard name={devData.name} imageSrc={devData.imageSrc} designation={devData.designation} gitLink={devData.gitLink}/></div>
+      ))}
+    </div>
+  )
+}
+
+export default Developers
