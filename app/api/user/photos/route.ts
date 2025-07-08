5e68e8fe8a769c8e66d4816be6d4620ae9cb6534
Md Arif <arif.20527@sscbs.du.ac.in>
2025-07-08 11:39:26 +0530

    prod fix 8

--- app/api/user/photos/route.ts
+++ app/api/user/photos/route.ts
@@ -5,8 +5,13 @@ import { NextRequest, NextResponse } from "next/server";
 //in use
 export async function POST(req:NextRequest){
   const session = await auth();
-  console.log(session);
-      return NextResponse.json("HEY Am back");
+  if(session){
+      const data = await UserFile.find({user:session.user?.id,delete:{$ne:true}}).populate({path:"file",match:{type:"image"}})
+      console.log(data)
+    //   return NextResponse.json(JSON.stringify({ success: true, data: data }));
+  }
+  //     console.log(data)
+  return NextResponse.json("HEY Am back");
   // if(session){
   //     const data = await UserFile.find({user:session.user?.id,delete:{$ne:true}}).populate({path:"file",match:{type:"image"}})
   //     console.log(data)
