a872a2f8e48f3f2ed5320a5242690c5e08dd87f2
the-sudip <mailofsudipdas@gmail.com>
2025-07-10 15:23:45 +0530

    landing page added to main structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,14 @@
-"use client";
+import LandingPage from "./landingpage/LandingPage"
+import React from 'react'
 
-import { useEffect, useRef } from "react";
-
-export default function Home() {
-  const fref = useRef(null);
-  function handle() {
-    // const f = new FormData();
-    // for (let i = 0; i < fref.current.files.length; i++) {
-    //   f.append("file", fref.current.files[i]);
-    // }
-    console.log(fref.current.files)
-    // fetch("/api/message", {
-    //   method: "POST",
-    //   body: f,
-    // });
-  }
+const page = () => {
   return (
-    <>
-      hello
-      <input type="file" ref={fref} multiple />
-      <button onClick={handle}>sub</button>
-    </>
-  );
+    <div>
+      <LandingPage />
+    </div>
+  )
 }
+
+export default page
+
+
